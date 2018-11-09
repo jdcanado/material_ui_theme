ffff28aa7a476d3dc6accfd4ef18be74dad78ae0
jdcanado <jdcanado@gmail.com>
2018-11-09 21:51:15 -0200

    Alteração do nome
    Alteração do nome do dashboard para ficar sem a palavra teste

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -21,7 +21,7 @@ const dashboardRoutes = [
   {
     path: "/dashboard",
     sidebarName: "Dashboard",
-    navbarName: "Jdcanado teste Dashboard",
+    navbarName: "Jdcanado Dashboard",
     icon: Dashboard,
     component: DashboardPage
   },
