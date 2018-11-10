26f8aab65f8d3b66bc23912687b4249209108ce4
jdcanado <jdcanado@gmail.com>
2018-11-10 00:19:00 -0200

    teste

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -21,7 +21,7 @@ const dashboardRoutes = [
   {
     path: "/dashboard",
     sidebarName: "Dashboard",
-    navbarName: "Jdcanado tes Dashboard",
+    navbarName: "Jdcanado Dashboard",
     icon: Dashboard,
     component: DashboardPage
   },
